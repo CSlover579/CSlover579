56a1354d17a6200621101ead82d179a47e92c677
Ragavan Shankarr Kalyanaraaman <ragavansk@outlook.com>
2024-04-26 21:08:12 -0700

    save

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 // use uuid::Uuid;
 
 fn main() {
-    let x: u32 = 5;
-    println!("Hello, world!");
-    println!("Success");
+    for number in 1..4{
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+    // let x: u32 = 5;
     // println!("Hello, world!");
-    // let id = Uuid::new_v4();
-    // println!("{}",id);
-    println!("Success once again");
-    println!("This is a test. Here is a number: {x}");
+    // println!("Success");
+    // // println!("Hello, world!");
+    // // let id = Uuid::new_v4();
+    // // println!("{}",id);
+    // println!("Success once again");
+    // println!("This is a test. Here is a number: {x}");
 }
